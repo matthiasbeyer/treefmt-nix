7b008f338f1c818ca9c2cad00598afc113ab3442
zimbatm <zimbatm@zimbatm.com>
2022-12-27 21:23:44 +0100

    flake-module: add default option
    Set to make it the default formatter for `nix fmt`

--- flake-module.nix
+++ flake-module.nix
@@ -3,25 +3,30 @@ let
   inherit (flake-parts-lib)
     mkPerSystemOption;
   inherit (lib)
+    mkEnableOption
     mkOption
     types;
 in
 {
   options = {
-    perSystem = mkPerSystemOption
-      ({ config, self', inputs', pkgs, system, ... }: {
-        options.treefmt = mkOption {
-          description = ''
-            Project-level treefmt configuration
-          '';
-          type = types.submoduleWith {
-            modules = (import ./.).all-modules pkgs;
-          };
+    perSystem = mkPerSystemOption ({ config, self', inputs', pkgs, system, ... }: {
+      options.treefmt = mkOption {
+        description = ''
+          Project-level treefmt configuration
+        '';
+        type = types.submoduleWith {
+          modules = ((import ./.).all-modules pkgs) ++ [{
+            xxx = mkEnableOption "set treefmt as the nix formatter";
+          }];
         };
-        config = {
-          checks.treefmt = config.treefmt.build.check self;
-          packages.treefmt = config.treefmt.build.wrapper;
-        };
+      };
+      config = {
+        checks.treefmt = config.treefmt.build.check self;
+        packages.treefmt = config.treefmt.build.wrapper;
+      } // (lib.mkIf config.treefmt.xxx {
+        # can be invoked with `nix fmt`
+        formatter = config.treefmt.build.wrapper;
       });
+    });
   };
 }
